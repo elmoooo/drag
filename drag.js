a91b76a67eafceb00fd03ca27edcef2f92de4669
elmoooo <elmoo.me@gmail.com>
2015-12-22 14:29:55 +0800

    change1 drag.js

--- drag.js
+++ drag.js
@@ -1,6 +1,9 @@
 /**
  * Created by admin on 2015/12/22.
  */
-function drag(){
-    
+function drag(obj){
+    var _div  = document.getElementById("div1")
+    obj.onmousedown = function () {
+
+    }
 }
